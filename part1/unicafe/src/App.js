81130b371ff84c5018e6b900b618b4fe342873a1
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-21 08:57:12 -0300

    release: part1 - Solving exercises 1.8 to 1.11
    Finishing unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,44 +1,54 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Show = ({ text, value, sign = "" }) => (
-  <p>
-    {text} {value} {sign}
-  </p>
+const StatisticLine = ({ text, value, sign = "" }) => (
+  <tr>
+    <td>{text}</td>
+    <td>
+      {value} {sign}
+    </td>
+  </tr>
 );
+
+const Statistics = ({ statistics }) => {
+  const { good, neutral, bad } = statistics;
+
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positivePercentage = total === 0 ? 0 : (good * 100) / total;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine
+              text="positive"
+              value={positivePercentage}
+              sign="%"
+            />
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  function calculateTotal() {
-    return good + neutral + bad;
-  }
-
-  function calculateAverage() {
-    console.log(calculateTotal());
-    const total = calculateTotal();
-
-    if (total == 0) {
-      return 0;
-    }
-
-    return (good - bad) / calculateTotal();
-  }
-
-  function calculatePositivePercentage() {
-    const total = calculateTotal();
-
-    if (total == 0) {
-      return 0;
-    }
-
-    return (good * 100) / calculateTotal();
-  }
 
   return (
     <div>
@@ -46,13 +56,8 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <h1>statistics</h1>
-      <Show text="good" value={good} />
-      <Show text="neutral" value={neutral} />
-      <Show text="bad" value={bad} />
-      <Show text="all" value={calculateTotal()} />
-      <Show text="average" value={calculateAverage()} />
-      <Show text="positive" value={calculatePositivePercentage()} sign="%" />
+
+      <Statistics statistics={{ good: good, neutral: neutral, bad: bad }} />
     </div>
   );
 };
