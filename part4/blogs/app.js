2429ff074d913905d5b908d3afa786236727a9a5
Arthur Detomi <arthurdetomi@gmail.com>
2025-07-31 09:30:41 -0300

    exercise 4.15 completed

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 
@@ -23,6 +24,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
