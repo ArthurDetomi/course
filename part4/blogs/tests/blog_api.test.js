1ce5adfc80d73e8845640aa6c4edacc6f6583186
Arthur Detomi <arthurdetomi@gmail.com>
2025-07-31 15:34:38 -0300

    Exercise 4.23 completed Parte 4 Completed

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -1,6 +1,7 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const helper = require("./test_helper");
+const bcrypt = require("bcrypt");
 const app = require("../app");
 const api = supertest(app);
 
@@ -12,7 +13,17 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 
   await User.deleteMany({});
-  await User.insertMany(helper.initialUsers);
+
+  const promisseArray = helper.initialUsers.map(async (user) => {
+    const passwordHash = await bcrypt.hash(user.password, 10);
+    const userToSave = new User({
+      username: user.username,
+      passwordHash: passwordHash,
+    });
+    return userToSave.save();
+  });
+
+  await Promise.all(promisseArray);
 });
 
 describe("when there is initially some blogs saved", () => {
@@ -32,8 +43,16 @@ describe("addition of a new blog", () => {
       likes: 12,
     };
 
+    const loginResponse = await api.post("/api/login").send({
+      username: "lucia",
+      password: "senha123",
+    });
+
+    const tokenJwt = loginResponse.body.token;
+
     await api
       .post("/api/blogs")
+      .set("Authorization", `Bearer ${tokenJwt}`)
       .send(newBlog)
       .expect(201)
       .expect("Content-Type", /application\/json/);
@@ -52,8 +71,16 @@ describe("addition of a new blog", () => {
       url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     };
 
+    const loginResponse = await api.post("/api/login").send({
+      username: "lucia",
+      password: "senha123",
+    });
+
+    const tokenJwt = loginResponse.body.token;
+
     const blogSaved = await api
       .post("/api/blogs")
+      .set("Authorization", `Bearer ${tokenJwt}`)
       .send(newBlog)
       .expect(201)
       .expect("Content-Type", /application\/json/);
@@ -67,7 +94,18 @@ describe("addition of a new blog", () => {
       likes: 13,
     };
 
-    await api.post("/api/blogs").send(newBlog).expect(400);
+    const loginResponse = await api.post("/api/login").send({
+      username: "lucia",
+      password: "senha123",
+    });
+
+    const tokenJwt = loginResponse.body.token;
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .set("Authorization", `Bearer ${tokenJwt}`)
+      .expect(400);
 
     const blogsAtEnd = await helper.blogsInDb();
 
@@ -82,23 +120,6 @@ test("blogs have id property with 'id'", async () => {
 
   expect(blogs[0].id).toBeDefined();
 }, 100000);
-
-describe("deletion of a blog", () => {
-  test("succeeds with status code 204 if id is valid", async () => {
-    const blogsAtStart = await helper.blogsInDb();
-    const blogToDelete = blogsAtStart[0];
-
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-    const blogsAtEnd = await helper.blogsInDb();
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-    const titles = blogsAtEnd.map((b) => b.title);
-
-    expect(titles).not.toContain(blogToDelete.title);
-  });
-});
 
 describe("update of a blog", () => {
   test("information of blog must be updated", async () => {
