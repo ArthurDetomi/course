7b470fc3db2bbe29a3e8092a8c5b9bfecf2f3f68
Arthur Detomi <arthurdetomi@gmail.com>
2025-07-31 10:37:55 -0300

    Exercise 4.17 completed

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,23 +1,29 @@
 const blogsRouter = require("express").Router();
 
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user");
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const { title, author, likes, url } = request.body;
+  const { title, author, likes, url, userId } = request.body;
+
+  const user = await User.findById(userId);
 
   const blog = new Blog({
     title,
     author,
     likes: likes ?? 0,
     url,
+    user: user.id,
   });
 
   const blogSaved = await blog.save();
+  user.blogs = user.blogs.concat(blogSaved._id);
+  await user.save();
 
   response.status(201).json(blogSaved);
 });
