0bbb4dfbe44ad4f21127607637091b28eae668ff
Arthur Detomi <arthurdetomi@gmail.com>
2025-08-05 15:03:03 -0300

    Part 7: Exercise 7.4

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -9,6 +9,8 @@ import {
   useNavigate,
 } from "react-router-dom";
 
+import { useField } from "./hooks/index";
+
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -87,9 +89,9 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("");
+  const author = useField("");
+  const info = useField("");
 
   const navigate = useNavigate();
 
@@ -115,27 +117,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
