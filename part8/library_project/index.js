db7fc68ea6751f82d4c02e177d01e5cc660b2bb9
Arthur Detomi <arthurdetomi@gmail.com>
2025-08-06 09:13:46 -0300

    Part 8: Exercise 8.3 completed

--- part8/library_project/index.js
+++ part8/library_project/index.js
@@ -103,19 +103,21 @@ const typeDefs = `
     published: Int
     author: String
     id: ID!
-    genres: [String!],
+    genres: [String!]
   }
 
   type Author {
     name: String
     id: ID!
     born: Int
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]
+    allAuthors: [Author!]
   }
 `;
 
@@ -124,6 +126,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (parent) => {
+      return books.filter((b) => b.author === parent.name).length;
+    },
   },
 };
 
